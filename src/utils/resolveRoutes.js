8ed146ccd68a06c3294f7dd4daf5aed2f5b0093c
¨enriqueorozcog¨ <enriqueorozcogg@gmail.com>
2020-06-05 21:22:45 -0600

    Se corrigio el error, ya muestra toda la info

--- src/utils/resolveRoutes.js
+++ src/utils/resolveRoutes.js
@@ -1,9 +1,9 @@
-const resolveRoutes = route => {
-    if (route.lenght <= 3) {
-        let validRoute = route === '/' ? route : '/:id';
-        return validRoute
+const resolveRoutes = (route) => {
+    if (route.length <= 3) {
+      let validRoute = route === '/' ? route : '/:id';
+      return validRoute;
     }
-    return route;
-};
-
-export default resolveRoutes
+    return `/${route}`;
+  };
+  
+  export default resolveRoutes;
